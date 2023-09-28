847d71165c74a425399f8a8050f212061d189924
Ttankkeo M <ttankkeo112@gmail.com>
2023-09-29 04:33:09 +0900

    update async reversion

--- js/localization/element.js
+++ js/localization/element.js
@@ -5,35 +5,32 @@
 class LocalizationElement extends HTMLElement {
     constructor() { super(); }
 
-    updateInnerHtml() {
+    async updateInnerHtml() {
         const hasKey = this.hasAttribute('key');
 
         if (hasKey) {
-            let key = this.getAttribute('key');
-            let parms = this.getAttribute('parms');
-            
-            fetch(`../../localizations/${language}.json`)
-            .then(reponse => reponse.json())
-            .then(json => {
-                let text = json[key];
-                
-                if (text === undefined) {
-                    this.innerHTML = json['undefined'];
+            const key   = this.getAttribute('key');
+            const parms = this.getAttribute('parms');
 
-                    throw 'Has not key in this localization json file.';
-                } else {
-                    // Handles parameters in the text.
-                    if (!(parms === null)) {
-                        parms = parms.split(',');
-                        
-                        for (let i = 0; i < parms.length; i++) {
-                            text = text.replace(`{${i}}`, parms[i]);
-                        }
-                    }
+            const json = await (await fetch(`../../localizations/${language}.json`)).json();
+            
+            let text = json[key];
+            if (text === undefined) {
+                this.innerHTML = json['undefined'];
+
+                throw 'Has not key in this localization json file.';
+            } else {
+                // Handles parameters in the text.
+                if (!(parms === null)) {
+                    parms = parms.split(',');
                     
-                    this.innerHTML = text;
+                    for (let i = 0; i < parms.length; i++) {
+                        text = text.replace(`{${i}}`, parms[i]);
+                    }
                 }
-            });
+                
+                this.innerHTML = text;
+            }
         } else {
             throw 'Has not key attribute in this element';
         }
